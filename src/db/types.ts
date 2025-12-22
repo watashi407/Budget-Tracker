6fe4697b619d2160e289357630654b6c0bbcf04e
watashi407 <hyperteddy70@gmail.com>
2025-12-22 11:09:57 +0800

    clean repo

--- src/db/types.ts
+++
@@ -1,8 +0,0 @@
-import type { InferSelectModel, InferInsertModel } from 'drizzle-orm';
-import { budgets, transactions } from './schema';
-
-export type Budget = InferSelectModel<typeof budgets>;
-export type NewBudget = InferInsertModel<typeof budgets>;
-
-export type Transaction = InferSelectModel<typeof transactions>;
-export type NewTransaction = InferInsertModel<typeof transactions>;
