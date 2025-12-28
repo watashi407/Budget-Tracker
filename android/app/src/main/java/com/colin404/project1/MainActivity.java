d68b7d5532c9b5741306feffe61cb288176f8b6f
watashi407 <hyperteddy70@gmail.com>
2025-12-28 18:14:07 +0800

    Add andriod

---
+++ android/app/src/main/java/com/colin404/project1/MainActivity.java
@@ -0,0 +1,5 @@
+package com.colin404.project1;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.colin404.project1',
+  appName: 'Project 1',
+  webDir: 'dist'
+};
+
+export default config;
